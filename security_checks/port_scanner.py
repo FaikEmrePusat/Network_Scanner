3015f6d5d460c227ad1ad319afe08db37d690d02
Faik Emre  Pusat <78548701+FaikEmrePusat@users.noreply.github.com>
2025-07-23 00:12:18 +0300

    Update port_scanner.py

--- security_checks/port_scanner.py
+++ security_checks/port_scanner.py
@@ -1,23 +1,26 @@
 import socket
 
+# Port tarama fonksiyonu
 def scan_ports(target_ip, start_port=1, end_port=65535):
     print(f"Scanning {target_ip} for open ports...")
 
-    open_ports = []
+    open_ports = [] # Açık portların listesi
 
+    # Portları tarama
     for port in range(start_port, end_port+1):
         try:
-            sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-            sock.settimeout(1)
-            resoult = socket.connect_ex((target_ip, port))
+            sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM) # TCP socket oluştur
+            sock.settimeout(1) # Zaman aşımı ayarı
+            resoult = socket.connect_ex((target_ip, port)) # Port'a bağlanmayı dene
 
             if resoult == 0:
-                open_ports.append(port)
+                open_ports.append(port) # Açık portları listeye ekle
                 print(f"Port {port} is open")
-            sock.close()
+            sock.close() # Socket'i kapat
         except Exception:
             pass
     
-    if len(open_ports) == 0:
+    if len(open_ports) == 0: # Eğer açık port yoksa
         print("No open ports found")
+
     return open_ports
